71f164f25e5a67affeceae2767b7b339cf3b7f7b
Амир Нурмухамбетов <thereug6@gmail.com>
2023-04-02 22:33:55 +0600

    offff

--- app/View/Layouts/page.ctp
+++ app/View/Layouts/page.ctp
@@ -11,7 +11,7 @@
         <?php endif; ?>
 
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
 
         <?php
             echo $this->Html->meta(
--- app/webroot/js/index.js
+++ app/webroot/js/index.js
@@ -20,3 +20,34 @@ const handleModalToggle = (id) => {
     const modal = document.getElementById(id);
     modal.dataset.toggled = modal.dataset.toggled === "true" ? "false" : "true";
 }
+
+
+/* -- Flex Gap Support Fallback -- */
+
+const flexGapSupported = () => {
+    const flex = document.createElement("div");
+    flex.style.display = "flex";
+    flex.style.flexDirection = "column";
+    flex.style.rowGap = "1px";
+
+    flex.appendChild(document.createElement("div"));
+    flex.appendChild(document.createElement("div"));
+
+    document.body.appendChild(flex);
+    const isSupported = flex.scrollHeight === 1;
+    flex.parentNode.removeChild(flex);
+
+    return isSupported;
+}
+
+// if (!flexGapSupported()) {
+//     const flexContainers = document.querySelectorAll(".flex-gap");
+
+//     flexContainers.forEach(container => {
+//         const flexItems = container.querySelectorAll(".flex-gap-item");
+
+//         flexItems.forEach(item => {
+//             item.style.marginBottom = "1rem";
+//         });
+//     });
+// }
